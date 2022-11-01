14bf5150d2811f380687d2df3ae4a3ff866417ca
tateno <gd28.c.tateno@gmail.com>
2022-11-02 01:45:24 +0900

    initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,34 @@
+import { Head } from "$fresh/runtime.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import Counter from "../islands/Counter.tsx";
+import { jokeHandler } from "./api/joke.ts";
+
+export const handler: Handlers<string> = {
+  async GET(req, ctx) {
+    const res: Response = await jokeHandler(req, ctx);
+    return ctx.render(await res.text());
+  },
+};
+
+export default function Home({ data }: PageProps<string>) {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div>
+        <img
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p>
+          Hello World.
+        </p>
+        <Counter start={3} />
+      </div>
+      <p>joke: {data}</p>
+    </>
+  );
+}
